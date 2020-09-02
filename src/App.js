76a4e65af5bb76685da1de1426c585feb2757054
raartist <rishabhzee98@gmail.com>
2020-09-02 09:00:16 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todo from './Todo'
 
 function App() {
+  const [todos,setTodos]=useState(['Take Dog out','Take the rubbish out','hell yeah!'])
+  const [input,setInput] = useState('')
+  console.log("❤",input)
+
+  const addTodo = (event)=>{
+    event.preventDefault()
+    console.log("💥","Todo Added!")
+    setTodos([...todos,input])
+    setInput('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello Learners! </h1>
+      <form>
+      <input value={input} onChange={event=>setInput(event.target.value)}/>
+      <button type="submit" disabled={!input} onClick={addTodo}>Add Todo</button>
+      </form>
+      <ul>{todos.map(todo=>(
+        <Todo text={todo}/>
+      ))}
+      </ul>
     </div>
   );
 }
